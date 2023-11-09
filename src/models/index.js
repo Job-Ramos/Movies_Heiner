0ae63516260257daa0dcdb35a6271933c0a82aab
Job Ramos <martinezramosheinerjob@gmail.com>
2023-11-09 08:41:08 -0500

    entregable3_Job

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+
+const Actor = require("./Actor");
+const Director = require("./Director");
+const Genre = require("./Genre");
+const Movie = require("./Movie");
+
+Movie.belongsToMany(Actor, {through: "MoviesActors"});
+Actor.belongsToMany(Movie, {through: "MoviesActors"});
+
+Movie.belongsToMany(Director,{through: "MoviesDirectors"});
+Director.belongsToMany(Movie,{through: "MoviesDirectors"});
+
+Movie.belongsToMany(Genre,{through: "MoviesGenres"});
+Genre.belongsToMany(Movie,{through: "MoviesGenres"});
+
+Director.belongsToMany(Movie,{through: "DirectorsMovies"});
+Movie.belongsToMany(Director,{through: "DirectorsMovies"});
